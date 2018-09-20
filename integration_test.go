c25e204d912204b52f16a5ad4d6735dcf33ef4e4
tsurubee <29603512+tsurubee@users.noreply.github.com>
2018-09-21 00:10:26 +0900

    Add fail test

--- integration_test.go
+++ integration_test.go
@@ -13,10 +13,10 @@ var (
 	integration = flag.Bool("integration", false, "run integration tests")
 )
 
-func loginByPassword(port int, t *testing.T) (*ssh.Client, *ssh.Session, error) {
+func loginByPassword(port int, password string) (*ssh.Client, *ssh.Session, error) {
 	sshConfig := &ssh.ClientConfig{
-		User: "tsurubee",
-		Auth: []ssh.AuthMethod{ssh.Password("testpass")},
+		User:            "tsurubee",
+		Auth:            []ssh.AuthMethod{ssh.Password(password)},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
@@ -30,12 +30,11 @@ func loginByPassword(port int, t *testing.T) (*ssh.Client, *ssh.Session, error) 
 		client.Close()
 		return nil, nil, err
 	}
-
 	return client, session, nil
 }
 
-func loginByPublicKey(port int, t *testing.T) (*ssh.Client, *ssh.Session, error) {
-	privateKeyBytes, err := ioutil.ReadFile("misc/testdata/client_keys/id_rsa")
+func loginByPublicKey(port int, keyPath string) (*ssh.Client, *ssh.Session, error) {
+	privateKeyBytes, err := ioutil.ReadFile(keyPath)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -45,8 +44,8 @@ func loginByPublicKey(port int, t *testing.T) (*ssh.Client, *ssh.Session, error)
 	}
 
 	sshConfig := &ssh.ClientConfig{
-		User: "tsurubee",
-		Auth: []ssh.AuthMethod{ssh.PublicKeys(privateKey)},
+		User:            "tsurubee",
+		Auth:            []ssh.AuthMethod{ssh.PublicKeys(privateKey)},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
@@ -74,20 +73,70 @@ func TestLoginByPassword(t *testing.T) {
 	if !*integration {
 		t.Skip()
 	}
-	client, _, err := loginByPassword(2222, t)
-	if err != nil {
-		t.Errorf("integration.TestLoginByPassword() error = %v, wantErr %v", err, nil)
+
+	tests := []struct {
+		name     string
+		password string
+		wantErr  bool
+	}{
+		{
+			name:     "success login",
+			password: "testpass",
+			wantErr:  false,
+		},
+		{
+			name:     "success login",
+			password: "failpass",
+			wantErr:   true,
+		},
 	}
-	defer client.Close()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, _, err := loginByPassword(2222, tt.password)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("integration.TestLoginByPassword() error = %v, wantErr %v", err, nil)
+				return
+			}
+			if client != nil {
+				client.Close()
+			}
+		})
+	}
 }
 
 func TestLoginByPublicKey(t *testing.T) {
 	if !*integration {
 		t.Skip()
 	}
-	client, _, err := loginByPublicKey(2222, t)
-	if err != nil {
-		t.Errorf("integration.TestLoginByPublicKey() error = %v, wantErr %v", err, nil)
+
+	tests := []struct {
+		name    string
+		keyPath string
+		wantErr bool
+	}{
+		{
+			name:    "success login",
+			keyPath: "misc/testdata/client_keys/id_rsa",
+			wantErr: false,
+		},
+		{
+			name:    "incorrect SSH key",
+			keyPath: "misc/testdata/client_keys/id_rsa_dummy",
+			wantErr: true,
+		},
 	}
-	defer client.Close()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, _, err := loginByPublicKey(2222, tt.keyPath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("integration.TestLoginByPublicKey() error = %v, wantErr %v", err, nil)
+				return
+			}
+			if client != nil {
+				client.Close()
+			}
+		})
+	}
 }
